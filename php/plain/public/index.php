c3a1c3edfe3bb49f4e6f3e04749d67709ccbdc57
David Buchmann <david.buchmann@liip.ch>
2025-05-12 11:13:15 +0200

    form to change image size

--- php/plain/public/index.php
+++ php/plain/public/index.php
@@ -4,12 +4,14 @@ use Imagine\Image\ImageInterface;
 
 require __DIR__.'/../vendor/autoload.php';
 
+$size = filter_input(INPUT_GET, 'size', FILTER_SANITIZE_NUMBER_INT) ?: 200;
+
 $imagine = new Imagine\Gd\Imagine();
 
-$size    = new Imagine\Image\Box(200, 200);
+$box = new Imagine\Image\Box($size, $size);
 
 $image = $imagine->open(__DIR__.'/../resources/deploio.png')
-    ->thumbnail($size, ImageInterface::THUMBNAIL_OUTBOUND)
+    ->thumbnail($box, ImageInterface::THUMBNAIL_OUTBOUND)
     ->get('png')
 ;
 
@@ -18,14 +20,28 @@ $image = $imagine->open(__DIR__.'/../resources/deploio.png')
 <head>
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <title>deplo.io plain PHP demo</title>
+    <style>
+        body {
+            margin: 0 auto;
+            padding-top: 60px;
+            max-width: 800px;
+        }
+    </style>
 </head>
 <body>
 <h1>Plain PHP Demo is working!</h1>
 <p>
     This simplistic PHP application demonstrates how to install a PHP application on deplo.io.
-    It uses the <code>imagine/imagine</code> library with the GD PHP extension <code>ext-gd</code> to scale an image to a different size.
+    It uses the <a href="https://github.com/php-imagine/Imagine" target="_blank"><code>imagine/imagine</code></a> library with the GD PHP extension <code>ext-gd</code> to scale an image to a different size.
     deplo.io uses the <code>composer.json</code> to know that it needs to provide <code>ext-gd</code> and then uses composer to install the dependencies.
 </p>
 <img title="deplo.io logo scaled in PHP" alt="deplo.io" src="data:image/png;base64,<?php echo base64_encode($image) ?>" />
+<p>This image is dynamically scaled with Imagine.
+    <form method="get">
+        <label for="size">Image size: </label>
+        <input type="number" id="size" name="size" min="20" max="2000" value="<?php echo $size ?>" /> px<br/>
+        <input type="submit" value="Scale Image"/>
+    </form>
+</p>
 </body>
 </html>
